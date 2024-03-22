6a6690033fd126bedc64ddcf9212c8064ad67dd9
Danh Nguyễn <danh010500@gmail.com>
2024-03-22 18:35:43 +0700

    commit script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -5,5 +5,7 @@ composer install --no-dev --working-dir=/var/www/html
 echo "Caching config..."
 php artisan config:cache
 
+php artisan key:generate
+
 echo "Caching routes..."
 php artisan route:cache
